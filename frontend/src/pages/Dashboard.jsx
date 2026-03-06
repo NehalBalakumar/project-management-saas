f301e35ca82b4bd5daffea63859da8aa7c48e88e
NehalBalakumar <nehalbalakumar7@gmail.com>
2026-03-06 21:11:22 +0530

    Day 2: Added login form and dashboard UI

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,43 @@
 function Dashboard() {
-  return <h2>Dashboard</h2>
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>Dashboard</h2>
+
+      <div style={{ display: "flex", gap: "20px", marginTop: "20px" }}>
+        
+        <div style={{
+          padding: "20px",
+          background: "#f4f4f4",
+          borderRadius: "8px",
+          width: "150px"
+        }}>
+          <h3>12</h3>
+          <p>Total Projects</p>
+        </div>
+
+        <div style={{
+          padding: "20px",
+          background: "#f4f4f4",
+          borderRadius: "8px",
+          width: "150px"
+        }}>
+          <h3>35</h3>
+          <p>Total Tasks</p>
+        </div>
+
+        <div style={{
+          padding: "20px",
+          background: "#f4f4f4",
+          borderRadius: "8px",
+          width: "150px"
+        }}>
+          <h3>5</h3>
+          <p>Team Members</p>
+        </div>
+
+      </div>
+    </div>
+  )
 }
 
 export default Dashboard
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,16 +1,46 @@
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 function Login() {
   const navigate = useNavigate()
 
-  const handleLogin = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
     navigate("/dashboard")
   }
 
   return (
-    <div>
-      <h2>Login Page</h2>
-      <button onClick={handleLogin}>Login</button>
+    <div style={{ padding: "40px" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div style={{ marginBottom: "10px" }}>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ padding: "8px", width: "200px" }}
+          />
+        </div>
+
+        <div style={{ marginBottom: "10px" }}>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" style={{ padding: "8px 16px" }}>
+          Login
+        </button>
+      </form>
     </div>
   )
 }
